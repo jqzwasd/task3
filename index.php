f9dbe32d1254298d5d3e89e0cc49d391c25a1af7
Timofey <timofejsokolov581@gmail.com>
2023-03-15 23:53:10 +0300

    Fist commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href = "style.css" rel="stylesheet" type="text/css">
+    <title>Document</title>
+</head>
+<header>
+</header>
+<body>
+<nav>
+        <ul>
+            <li>
+                <a href="index.html">Главная</a>
+            </li>
+            <li>
+                <a href="reg.html">Регистраиция</a>
+            </li>
+            <li>
+                <a href="auth.html">Авторизация</a>
+            </li>
+            <li>
+                <a href="hidden_page.html">Скрытая страница</a>
+            </li>
+        </ul>
+</nav>
+<main>
+    <div>
+        <h2>Это Главная</h2>
+    </div>
+    <form action="" method="get">
+            <input type="text" name = "login">
+            <input type="submit" value = "отправить">
+        </form>
+</main>
+    <footer>
+    Sokolov Timofey
+    </footer>
+</body>
+
+
+</html>
---
+++ layout.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+</head>
+<body>
+    <h1><?=$page_title?></h1>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.html">Главная</a>
+            </li>
+            <li>
+                <a href="reg.html">Регистраиция</a>
+            </li>
+            <li>
+                <a href="auth.html">Авторизация</a>
+            </li>
+            <li>
+                <a href="hidden_page.html">Скрытая страница</a>
+            </li>
+        </ul>
+</nav>
+    <main>
+    <div>
+        <h2>Регистраиция</h2>
+    </div>
+    <form action="" method="get">
+            <input type="text" name = "login">
+            <input type="submit" value = "отправить">
+        </form>
+    </main>
+</body>
+</html>
