c42705821ba74d90e4c87a9a4c359831d03d89d9
Timofey <timofejsokolov581@gmail.com>
2023-03-23 11:55:44 +0300

    Sixth commit

--- hidden_page.php
+++ hidden_page.php
@@ -8,6 +8,7 @@ $stmt = pdo() ->("SELECT * FROM users");
 $content = "";
 while ($row = $stmt -> fetch()){
     echo $row["id"];
+    $content =  $content . $row['id'] . " | ". $row['login'] . " | " . $row['password'] . "<br>";
 }
 include("content/layout.php");
 ?>
